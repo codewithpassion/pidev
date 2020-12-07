a252575ce3b6686c2980b0ad5b948b3dd4395c3b
codewithpassion <dominik.fretz@gmail.com>
2020-12-07 15:26:00 +1100

    update bootstrap

--- run-bootstrap.sh
+++ run-bootstrap.sh
@@ -7,6 +7,12 @@ else
     WORKSPACE=$1
 fi
 
+if [ ! -d "$WORKSPACE" ] 
+then
+    echo Workspace directory does not exist: $WORKSPACE.
+    echo Consider ruinning: run-bootstrap.sh <PATH>
+fi
+
 docker run --rm -it \
     -v ${WORKSPACE}:/data \
     -v ${HOME}/.local/pidev/:/home/user/.conan/data \
